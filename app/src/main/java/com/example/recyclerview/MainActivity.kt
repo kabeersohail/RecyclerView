5ff8dddc1a476e2bbdff04984a37fd5dc94cb0d6
kabeersohail <kabeersohel@gmail.com>
2023-09-29 01:00:28 +0530

    Restructured the app to utilize a single activity with a navigation graph and fragment for improved organization and navigation

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,32 +1,13 @@
 package com.example.recyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import com.example.recyclerview.database.dao.CountryLastUpdateTimestampDao
-import com.example.recyclerview.database.dao.UniversityDao
 import com.example.recyclerview.databinding.ActivityMainBinding
-import com.example.recyclerview.extensions.TAG
-import com.example.recyclerview.network.UniversityApi
-import com.example.recyclerview.repository.UniversityRepository
-import com.example.recyclerview.viewmodelfactory.UniversityViewModelFactory
-import com.example.recyclerview.viewmodels.UniversityViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var universityApi: UniversityApi
-
-    @Inject
-    lateinit var universityDao: UniversityDao
-
-    @Inject
-    lateinit var countryLastUpdateTimestampDao: CountryLastUpdateTimestampDao
 
     private lateinit var binding: ActivityMainBinding
 
@@ -34,18 +15,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        val universityRepository = UniversityRepository(universityApi, universityDao, countryLastUpdateTimestampDao)
-        val viewModelFactory = UniversityViewModelFactory(universityRepository)
-        val viewModel = ViewModelProvider(this, viewModelFactory)[UniversityViewModel::class.java]
-
-        binding.fetchUniversities.setOnClickListener {
-            viewModel.getUniversities("India")
-        }
-
-        viewModel.getUniversitiesLiveData().observe(this) {
-            Log.d(TAG, "$it")
-        }
-
     }
 }
---
+++ app/src/main/java/com/example/recyclerview/views/HomeFragment.kt
@@ -0,0 +1,60 @@
+package com.example.recyclerview.views
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.recyclerview.database.dao.CountryLastUpdateTimestampDao
+import com.example.recyclerview.database.dao.UniversityDao
+import com.example.recyclerview.databinding.FragmentHomeBinding
+import com.example.recyclerview.extensions.TAG
+import com.example.recyclerview.network.UniversityApi
+import com.example.recyclerview.repository.UniversityRepository
+import com.example.recyclerview.viewmodelfactory.UniversityViewModelFactory
+import com.example.recyclerview.viewmodels.UniversityViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    @Inject
+    lateinit var universityApi: UniversityApi
+
+    @Inject
+    lateinit var universityDao: UniversityDao
+
+    @Inject
+    lateinit var countryLastUpdateTimestampDao: CountryLastUpdateTimestampDao
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, null, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val universityRepository = UniversityRepository(universityApi, universityDao, countryLastUpdateTimestampDao)
+        val viewModelFactory = UniversityViewModelFactory(universityRepository)
+        val viewModel = ViewModelProvider(this, viewModelFactory)[UniversityViewModel::class.java]
+
+        binding.fetchUniversities.setOnClickListener {
+            viewModel.getUniversities("India")
+        }
+
+        viewModel.getUniversitiesLiveData().observe(requireActivity()) {
+            Log.d(TAG, "$it")
+        }
+
+    }
+
+}
